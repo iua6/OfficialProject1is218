5fcaa3151c4e7729f1015283cc4fd8ceeb33f081
iua6 <90913063+iua6@users.noreply.github.com>
2022-03-29 15:59:11 -0400

    Update simple_pages_test.py

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -48,10 +48,9 @@ def test_request_page6(client):
     """This makes the index page"""
     response = client.get("/page6")
     assert response.status_code == 200
-
-
+    
 def test_request_page7(client):
-  """This makes the index page"""
+    """This makes the index page"""
     response = client.get("/page7")
     assert response.status_code == 200
 
